a925240cbda9d2979687db5222138c538f7fba8e
2R <63011073+RichalRambaran@users.noreply.github.com>
2020-05-23 22:47:00 +0200

    Troop component methods created

--- src/com/company/Sergeant.java
+++ src/com/company/Sergeant.java
@@ -4,4 +4,16 @@ public class Sergeant extends Troop {
     public Sergeant(String name) {
         super(name);
     }
+
+    public void orderToAim() {
+
+    }
+
+    public void orderToShoot() {
+
+    }
+
+    public void orderToReload() {
+
+    }
 }
--- src/com/company/Soldier.java
+++ src/com/company/Soldier.java
@@ -4,4 +4,12 @@ public class Soldier extends Troop {
     public Soldier(String name) {
         super(name);
     }
+
+    public void grabMachineGun() {
+
+    }
+
+    public void grabRifle() {
+
+    }
 }
