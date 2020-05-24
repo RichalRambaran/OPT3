758c8084acf991d3e21a5af3130eabfa00077d9a
2R <63011073+RichalRambaran@users.noreply.github.com>
2020-05-24 22:52:15 +0200

    soldier refactors

--- src/com/company/Soldier.java
+++ src/com/company/Soldier.java
@@ -10,8 +10,7 @@ public class Soldier extends Troop {
     public void grabMachineGun() {
         int index = 0;
         if (FireArmDeposit.getFireArmDeposit().getMachineGunCompartment().getMachineGuns().get(index) != null) {
-            fireArm = FireArmDeposit.getFireArmDeposit().getMachineGunCompartment().getMachineGuns().get(index);
-            FireArmDeposit.getFireArmDeposit().getMachineGunCompartment().getMachineGuns().remove(index);
+            obtainMachineGun(index);
             System.out.println(getName() + " has grabbed a machine gun");
         }
         else {
@@ -19,11 +18,15 @@ public class Soldier extends Troop {
         }
     }
 
+    private void obtainMachineGun(int index) {
+        fireArm = FireArmDeposit.getFireArmDeposit().getMachineGunCompartment().getMachineGuns().get(index);
+        FireArmDeposit.getFireArmDeposit().getMachineGunCompartment().getMachineGuns().remove(index);
+    }
+
     public void grabRifle() {
         int index = 0;
         if (FireArmDeposit.getFireArmDeposit().getRifleCompartment().getRifles().get(index) != null) {
-            fireArm = FireArmDeposit.getFireArmDeposit().getRifleCompartment().getRifles().get(index);
-            FireArmDeposit.getFireArmDeposit().getRifleCompartment().getRifles().remove(index);
+            obtainRifle(index);
             System.out.println(getName() + " has grabbed a rifle");
         }
         else {
@@ -31,11 +34,16 @@ public class Soldier extends Troop {
         }
     }
 
+    private void obtainRifle(int index) {
+        fireArm = FireArmDeposit.getFireArmDeposit().getRifleCompartment().getRifles().get(index);
+        FireArmDeposit.getFireArmDeposit().getRifleCompartment().getRifles().remove(index);
+    }
+
     public void shootBurst(Target target) {
         String startingText = getName() + " couldn't shoot the target because";
-        if (target.getDistance() <= fireArm.getMaxRange()) {
-            if (target.getAlive()) {
-                if (fireArm.getMagazine().getCurrentAmmunition() >= fireArm.getBulletsPerBurst()) {
+        if (isInRange(target)) {
+            if (isAlive(target)) {
+                if (hasEnoughAmmunition()) {
                     carryOutShot(target);
                     System.out.println(getName() + " successfully shot the target");
                 }
@@ -50,6 +58,24 @@ public class Soldier extends Troop {
         else {
             System.out.println(startingText + " the target is out of range.");
         }
+    }
+
+    private Boolean isInRange(Target target) {
+        boolean isInRange;
+        isInRange = target.getDistance() <= fireArm.getMaxRange();
+        return isInRange;
+    }
+
+    private Boolean isAlive(Target target) {
+        boolean isAlive;
+        isAlive = target.getAlive();
+        return isAlive;
+    }
+
+    private Boolean hasEnoughAmmunition() {
+        boolean hasEnoughAmmunition;
+        hasEnoughAmmunition = fireArm.getMagazine().getCurrentAmmunition() >= fireArm.getBulletsPerBurst();
+        return hasEnoughAmmunition;
     }
 
     private void carryOutShot(Target target) { // extracting methods to prevent large method
